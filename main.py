867ed50d697193da075017bfb1043b96d0f70abb
jurchiks33 <jurisslisans@gmail.com>
2024-02-13 11:22:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+def start_bot():
+    print("Bot started with the following parameters:")
+    print(f"Crypto Pair: {pair_entry.get()}")
+    print(f"Buy Treshold: {buy_treshold_entry.get()}")
+    print(f"Sell Treshold: {sell_treshold_entry.get()}")
+
+#Main Window.
